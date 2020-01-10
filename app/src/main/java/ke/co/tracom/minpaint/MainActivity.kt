0cf64629724282b34eaadae5d50dad9b5c423f83
Kennedy Kariuki <mihangokennedy@gmail.com>
2020-01-10 12:11:33 +0300

    aalow drawing in a canvas

--- app/src/main/java/ke/co/tracom/minpaint/MainActivity.kt
+++ app/src/main/java/ke/co/tracom/minpaint/MainActivity.kt
@@ -1,12 +1,18 @@
 package ke.co.tracom.minpaint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvasView)
     }
 }
